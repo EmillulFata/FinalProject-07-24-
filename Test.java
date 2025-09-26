f87504f714cfa0964e48355d3bfa2744b1cf79af
ridoq <ridoq444@gmail.com>
2025-09-26 16:14:38 +0700

    melakukan perubahan pada file supaya bisa di pull

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
   public static void main(String[] args) {
     System.out.println("Halo Ridho,Ini adalah baris pertama");
+    System.out.println("Halo Emil Martinez");
   }
 }
